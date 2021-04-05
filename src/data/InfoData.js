f092554ba109c6641636eac6a5ba4fff0ea67ab5
TheDarkInstinct <77352616+TheDarkInstinct@users.noreply.github.com>
2021-04-05 17:09:23 +0530

    second commit

---
+++ src/data/InfoData.js
@@ -0,0 +1,25 @@
+import ImageOne from '../images/interior-2.jpg'
+import ImageTwo from '../images/interior-1.jpg'
+
+
+
+export const InfoData = {
+    heading: 'Explore our beautiful homes',
+    paragrapgOne: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry If your project grows in size, this option is available.',
+    paragraphTwo: "If your project grows in size, this option is available. This method has the trade-off that it takes a long time to install the package. Suitable for MeteorJS, Gatsbyjs etc.",
+    buttonLabel: 'View Homes',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+};
+
+
+export const InfoDataTwo = {
+    heading: 'Modern Designs',
+    paragrapgOne: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    paragraphTwo: "If your project grows in size, this option is available",
+    buttonLabel: 'View Homes',
+    image: ImageTwo,
+    reverse: true,
+    delay: 100
+};
---
+++ src/data/SliderData.js
@@ -0,0 +1,43 @@
+import ImageOne from '../images/image-7.jpg';
+import ImageTwo from '../images/house-2.jpg';
+import ImageThree from '../images/image-6.jpg';
+import ImageFour from '../images/image-5.jpg';
+
+
+export const SliderData = [
+    {
+        title: 'Luxury Villa in Bali, Indonesia',
+        price: "$4,280,000",
+        path: '/homes',
+        label: 'View Home',
+        image: ImageOne,
+        alt: 'House'
+    },
+
+    {
+        title: 'Luxury Calabasas, California',
+        price: "$4,280,000",
+        path: '/homes',
+        label: 'View Home',
+        image: ImageTwo,
+        alt: 'House'
+    },
+
+    {
+        title: 'House in San Jose, Costa Rica',
+        price: "$3,180,000",
+        path: '/homes',
+        label: 'View Home',
+        image: ImageThree,
+        alt: 'House'
+    },
+
+    {
+        title: 'House in Housten, Texas',
+        price: "$5,380,000",
+        path: '/homes',
+        label: 'View Home',
+        image: ImageFour,
+        alt: 'House'
+    },
+]
